3eac53954bc5539e4d293967942529f8e019d09d
风云神舞 <826947258@qq.com>
2016-03-26 16:26:19 +0800

    project init

---
+++ src/main/java/com/nsh/users/controller/IndexController.java
@@ -0,0 +1,37 @@
+package com.nsh.users.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+	@RequestMapping(value = "/admin-manage/index", method = RequestMethod.GET)
+	public String showIndex() {
+		return null;
+	}
+
+	@RequestMapping(value = "/admin-login", method = RequestMethod.GET)
+	public String showAdminLogin() {
+		return null;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "home";
+	}
+}
