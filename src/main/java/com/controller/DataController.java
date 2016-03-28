aab1a52270f3d8b799abb1839747333b2576ad9d
风云神舞 <826947258@qq.com>
2016-03-28 22:01:16 +0800

    connect mysql by jdbcTemplate

--- src/main/java/com/controller/DataController.java
+++ src/main/java/com/controller/DataController.java
@@ -1,10 +1,15 @@
 package com.controller;
 
 import com.domain.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,24 +19,30 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/charts")
 public class DataController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@RequestMapping(value = "getData")
 	@ResponseBody
 	public List<Point> getData() {
 		//TODO:介入动态数据
-		//现在造假数据
+		//使用jdbcTemplate对象操作数据库
 		List<Point> dataList = new ArrayList<Point>();
 
-		Point point1 = new Point(1.0, 2.0 , "p1");
-		Point point2 = new Point(1.0, 1.0 , "p1");
-		Point point3 = new Point(2.0, 2.0 , "p1");
-		Point point4 = new Point(3.0, 2.0 , "p1");
-		Point point5 = new Point(4.0, 4.0 , "p1");
+		String sql = "select * from table where id = ?";
 
-		dataList.add(point1);
-		dataList.add(point2);
-		dataList.add(point3);
-		dataList.add(point4);
-		dataList.add(point5);
+
+		//使用方法可以搜索JdbcTemplate的使用，这里只是一个例子，我没跑
+		jdbcTemplate.query(sql, new RowMapper<Object>() {
+			public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+				Point point = new Point();
+				point.setX(resultSet.getDouble("x"));
+				point.setX(resultSet.getDouble("y"));
+				point.setId(resultSet.getString("id"));
+				return point;
+			}
+		});
+
 
 		return dataList;
 	}
